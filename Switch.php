ae843011e3e8776529a1905ef6eed21c2672c10e
dhruvi1112 <73457500+dhruvi1112@users.noreply.github.com>
2021-09-21 12:24:41 +0530

    Add file via upload

---
+++ Switch.php
@@ -0,0 +1,30 @@
+<?php
+
+$a=5;
+	switch($a)
+	{
+		case 1:
+			echo 'Today is Monday';
+			break;
+		case 2:
+			echo 'Today is Tuesday';
+			break;
+		case 3:
+			echo 'Today is Wednesday';
+			break;
+		case 4:
+			echo 'Today is Thursday';
+			break;
+		case 5:
+			echo 'Today is Friday';
+			break;
+		case 6:
+			echo 'Today is saturday';
+			break;
+		case 7:
+			echo 'Today is Sunday';
+			break;
+		default:
+			echo 'choose 1-7';
+			break;
+	}
