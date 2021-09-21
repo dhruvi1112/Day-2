847e19100937e705e82942008073c5fbb189502f
dhruvi1112 <73457500+dhruvi1112@users.noreply.github.com>
2021-09-21 12:23:42 +0530

    Add file via upload

---
+++ Intro.php
@@ -0,0 +1,50 @@
+<?php ?>
+<html>
+<body>
+ <h1>Welcome to my World!!</h1>
+
+<?php 
+	echo 'HI!! I am Niyati.</br></br>';
+	echo 'Doing PHP code.</br></br>';
+?>
+<style>
+table,th,td{
+	border: 1px solid black;
+	border-collapse : collapse;
+
+}
+th,td{
+	padding: 15px;
+	
+}
+table{
+	width : 50%;
+}
+</style>
+<table>
+<tr>
+<th colspan = '3' text-align : 'center'>Information</th>
+</tr>
+<tr>
+<th>Name</th>
+<th>Age</th>
+<th>Subject</th>
+</tr>
+<tr>
+<td>Niyati Joshi</td>
+<td>21</td>
+<td>PHP</td>
+</tr>
+<tr>
+<td>Dhruvi Shah</td>
+<td>21</td>
+<td>Python</td>
+</tr>
+<tr>
+<td>Shruti Pandya</td>
+<td>21</td>
+<td>JAVA</td>
+</tr>
+</table>
+</body>
+</html>
